d3a5d17a07c3abd5c0d3a550868042b4a330f4a6
Srikanth <golusulasrikanth91@gmail.com>
2023-06-19 16:38:53 +0530

    As A lead i have created Hybrid testNG Framework project include pageobject model page factory desing  patterns,i have also automated the few test cases like login,registers,search fuctionality

--- src/main/java/com/tutorialsninja/qa/listeners/MyListeners.java
+++ src/main/java/com/tutorialsninja/qa/listeners/MyListeners.java
@@ -3,15 +3,10 @@ package com.tutorialsninja.qa.listeners;
 import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
-
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.io.FileHandler;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
